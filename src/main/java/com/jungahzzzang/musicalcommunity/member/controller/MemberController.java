781d05de45063d1d54f70472ba72597dbe86bb53
jungahzzzang <jungahzzzang@gmail.com>
2022-06-26 22:41:46 +0900

    소셜 로그인 구현중

--- src/main/java/com/jungahzzzang/musicalcommunity/member/controller/MemberController.java
+++ src/main/java/com/jungahzzzang/musicalcommunity/member/controller/MemberController.java
@@ -1,14 +1,18 @@
 package com.jungahzzzang.musicalcommunity.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/member")
 public class MemberController {
 
-    @GetMapping("/member/login")
+    @GetMapping("login")
     public String loginMember(){
         return "member/login";
     }
